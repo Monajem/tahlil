727887860a270815d4391329deeca8f20af3118a
thebigdude <sinraf96@gmail.com>
2019-01-02 03:21:01 +0330

    Update forgotten files in last commit

--- main/views.py
+++ main/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def login(request):
+    return render(request, 'main/login.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
